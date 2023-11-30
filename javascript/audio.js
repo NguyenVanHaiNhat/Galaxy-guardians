34bf70ef194f358f638d455a83e55eed0cb07e08
nhat0120230 <nhatnguyen221020032@gmail.com>
2023-11-30 14:32:20 +0700

    thêm nhạc cho game

--- javascript/audio.js
+++ javascript/audio.js
@@ -1,7 +1,6 @@
 function playShootSound() {
     const shootSound = new Audio('./sounds/shoot.wav');
 
-    // Lắng nghe sự kiện 'ended' để có thể chơi âm thanh nhiều lần
     shootSound.addEventListener('ended', function() {
         this.currentTime = 0;
     }, false);
@@ -9,14 +8,12 @@ function playShootSound() {
     shootSound.play();
 }
 
-// Thêm sự kiện cho sự kiện tải trang
 window.addEventListener('load', function() {
-    // Chơi âm nhạc nền khi trang được tải
     playBackgroundMusic();
 });
 
 function playBackgroundMusic() {
     const backgroundMusic = new Audio('./sounds/backgruondsound.wav');
-    backgroundMusic.volume = 0.5; // Điều chỉnh âm lượng theo nhu cầu
+    backgroundMusic.volume = 0.5;
     backgroundMusic.play();
 }
