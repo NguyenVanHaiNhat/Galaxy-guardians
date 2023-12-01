9ddbff98c40f516fb0788009a211abe728585499
nhat0120230 <nhatnguyen221020032@gmail.com>
2023-12-01 15:17:32 +0700

    thay đổi màn 2

--- javascript/stage2.js
+++ javascript/stage2.js
@@ -4,17 +4,25 @@ function spawnEnemyStage2() {
     let enemyWidth = 20;
     let enemyHeight = 20;
     let enemySpeed = 0.5;
+    let spawnInterval = 20000; // Thời gian giữa mỗi lần spawn (10s)
+    let enemiesToSpawn = 20; // Số lượng kẻ địch mỗi lần spawn
 
-    for (let i = 0; i < 5; i++) {
-        let enemy = {
-            x: i * rowWidth + (rowWidth - enemyWidth) / 2,
-            y: rowY,
-            width: enemyWidth,
-            height: enemyHeight,
-            speed: enemySpeed,
-            directionX: -1,
-            directionY: 1,
-        };
-        enemies.push(enemy);
+    function spawnEnemies() {
+        for (let i = 0; i < enemiesToSpawn; i++) {
+            let enemy = {
+                x: i * rowWidth + (rowWidth - enemyWidth) / 2,
+                y: rowY,
+                width: enemyWidth,
+                height: enemyHeight,
+                speed: enemySpeed,
+                directionX: -1,
+                directionY: 1,
+            };
+            enemies.push(enemy);
+        }
     }
+
+    // Gọi hàm spawnEnemies ngay từ lúc bắt đầu và sau mỗi khoảng thời gian spawnInterval
+    spawnEnemies();
+    setInterval(spawnEnemies, spawnInterval);
 }
